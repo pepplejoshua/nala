e644be4b1f67f20af8a36573c0b3f15193b4d037
Iwarilama <pepplejoshuatams@gmail.com>
2021-07-02 09:09:47 -0600

    Implemented let and return statements with tests

--- parser/parser.go
+++ parser/parser.go
@@ -1,20 +1,22 @@
 package parser
 
 import (
+	"fmt"
 	"nala/ast"
 	"nala/lexer"
 	"nala/token"
 )
 
 type Parser struct {
-	l *lexer.Lexer
+	l      *lexer.Lexer
+	errors []string
 
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{l: l, errors: []string{}}
 
 	// this sets both curToken and peekToken
 	p.nextToken()
@@ -48,6 +50,8 @@ func (p *Parser) ParseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -68,8 +72,37 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	}
 
 	// TODO: Expressions will be handled here. We skip over it for now
+	// there is a bug here. If u skip the terminating ';' it enters an infinite loop
 	for !p.curTokenIs(token.SEMICOLON) {
-		p.nextToken()
+		if !p.curTokenIs(token.EOF) {
+			p.nextToken()
+		} else {
+			break
+		}
+	}
+
+	if !p.expectCur(token.SEMICOLON) {
+		return nil
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	// TODO: Expressions will be handled here. We skip over it for now
+	// there is a bug here. If u skip the terminating ';' it enters an infinite loop
+	for !p.curTokenIs(token.SEMICOLON) {
+		if !p.curTokenIs(token.EOF) {
+			p.nextToken()
+		} else {
+			break
+		}
+	}
+
+	if !p.expectCur(token.SEMICOLON) {
+		return nil
 	}
 
 	return stmt
@@ -80,6 +113,16 @@ func (p *Parser) expectPeek(expectedType token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(expectedType)
+		return false
+	}
+}
+
+func (p *Parser) expectCur(expectedType token.TokenType) bool {
+	if p.curTokenIs(expectedType) {
+		return true
+	} else {
+		p.curError(expectedType)
 		return false
 	}
 }
@@ -90,4 +133,18 @@ func (p *Parser) curTokenIs(expectedType token.TokenType) bool {
 
 func (p *Parser) peekTokenIs(expectedType token.TokenType) bool {
 	return expectedType == p.peekToken.Type
+}
+
+func (p *Parser) Errors() []string { return p.errors }
+
+func (p *Parser) peekError(t token.TokenType) {
+	err := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, err)
+}
+
+func (p *Parser) curError(t token.TokenType) {
+	err := fmt.Sprintf("expected current token to be %s, got %s instead",
+		t, p.curToken.Type)
+	p.errors = append(p.errors, err)
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -20,6 +20,8 @@ func TestLetStatements(t *testing.T) {
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParseErrors(t, p)
+
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -67,4 +69,50 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	}
 
 	return true
+}
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+	return 5;
+	return 10;
+	return 20000000;
+	`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParseErrors(t, p)
+
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatement. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral() not 'return. got=%q", returnStmt.TokenLiteral())
+		}
+	}
+}
+
+func checkParseErrors(t *testing.T, p *Parser) {
+	errs := p.Errors()
+
+	if len(errs) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errs))
+
+	for _, err := range errs {
+		t.Errorf("parser error: %q", err)
+	}
+	t.FailNow()
 }
