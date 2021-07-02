49fa5aa49273d72f851b3ed6983ad1df8410a37f
Iwarilama <pepplejoshuatams@gmail.com>
2021-07-01 19:40:11 -0600

    Created REPL

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"nala/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is Nala programming language!\n", user.Username)
+	fmt.Printf("Feel free to type commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
--- repl/repl.go
+++ repl/repl.go
@@ -1,1 +1,31 @@
 package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"nala/lexer"
+	"nala/token"
+)
+
+const PROMPT = "=> "
+
+func Start(in io.Reader, out io.Writer) {
+	// this wraps the input with a Buffer that we can Scan?
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
